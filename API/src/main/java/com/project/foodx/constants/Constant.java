978ba4486df19c415d502af20ea2ba177f8578a4
kaustubhb <kaustubhbawa123@gmail.com>
2021-03-05 23:15:31 +0530

    Delete API directory

--- API/src/main/java/com/project/foodx/constants/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.project.foodx.constants;
-
-public class Constant {
-
-    public static final String API_SECRET_KEY = "kdjfhsfjhsdkfjhsd9879fiuerwieuh87987dkjfhsdkjfhsdkjfhksdjfhskdj3";
-
-
-
-}
